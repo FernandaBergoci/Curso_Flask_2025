93c34d00ae4c9d1c78b0399257559dd7fb93722e
Fernanda Bergoci <fernandabergoci@gmail.com>
2025-05-25 22:51:17 -0300

    Formulário simples no flask

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 os.system('cls')
 
 # coding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask("projeto")
 
 # Rota Raiz
@@ -21,4 +21,21 @@ def ola_mundo():
 @app.route("/teste/<variavel>")
 def funcao_teste(variavel=""):
     return "Nova rota teste<br>Variável: {}".format(variavel), 200
+
+# Rota formulário
+@app.route("/form")
+def form():
+    return render_template("form.html"), 200
+
+# Rota tratamento do formulário
+@app.route("/form_recebe", methods = ["GET","POST"])
+def form_recebe():
+    nome = ""
+    if request.method == "POST":
+        nome = request.form["nome"]
+        return "Nome: {}".format(nome), 200
+    else:
+        return "Não pode chamar direto no GET",200
+
+
 app.run()
